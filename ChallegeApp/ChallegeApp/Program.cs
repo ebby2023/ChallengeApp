318ab3b3358fa8e55514b023fb270216b4e722e9
Ebby2023 <zibello2010@gmail.com>
2023-02-12 20:47:21 +0000

    Day 4

---
+++ ChallegeApp/ChallegeApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is my first project
+// Implementing veriables 
+
+var age = 17;
+var name = "Ewa";
+var sex = "female";
+
+// implementing loop for
+
+if (age < 18 && name == "Ewa" && sex == "female")
+{
+    Console.WriteLine($"Your name is {name}. You are {sex}. You are not adult yet.");
+}
+else if (age >= 18 && name == "Ewa" && sex == "female")
+{
+    Console.WriteLine($"Your name is {name}.You are {sex}. You are adult");
+}
+
