c8911b9208e4e00026f9e4b10a6ca10f26967016
Ebby2023 <zibello2010@gmail.com>
2023-02-15 21:06:35 +0000

    Dzien 5

--- ChallegeApp/ChallegeApp/Program.cs
+++ ChallegeApp/ChallegeApp/Program.cs
@@ -1,27 +1,80 @@
-﻿string name = "Ewa";
+﻿int number = 65658787;
 
-bool female = false;
+string numberAsString = number.ToString();
 
-var age = 33;
+char[] letters = numberAsString.ToCharArray();
 
+Console.WriteLine($" Liczba {number} - ilosc cyfr w liczbie ");
+Console.WriteLine( );
 
-if (female == true && age < 30)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+foreach (char letter in letters)
+
 {
-    Console.WriteLine("You are woman.You are under 30.");
+    if (letter == '0')
+    {
+        counter0++;
+
+    }
+    if (letter =='1') 
+    {
+        counter1++;
+    }
+    if (letter == '2')
+    {
+        counter2++;
+    }
+    if (letter == '3')
+    {
+        counter3++;
+    }
+    if (letter == '4')
+    {
+        counter4++;
+    }
+    if (letter == '5')
+    {
+        counter5++;
+
+    }
+    if (letter == '6')
+    {
+        counter6++;
+    }
+    if (letter == '7')
+    {
+        counter7++;
+    }
+    if (letter == '8')
+    {
+        counter8++;
+    }
+    if(letter == '9')
+    {
+        counter9++;
+    }
 }
 
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("You are Ewa. You are 33.");
-}
+Console.WriteLine("0 ==> " + counter0);
+Console.WriteLine("1 ==> " + counter1);
+Console.WriteLine("2 ==> " + counter2);
+Console.WriteLine("3 ==> " + counter3);
+Console.WriteLine("4 ==> " + counter4);
+Console.WriteLine("5 ==> " + counter5);
+Console.WriteLine("6 ==> " + counter6);
+Console.WriteLine("7 ==> " + counter7);
+Console.WriteLine("8 ==> " + counter8);
+Console.WriteLine("9 ==> " + counter9);
 
-else if(female == true && age <18)
-{
-    Console.WriteLine("You are a woman. You are under 18.");
-}
 
-else
-{
-    Console.WriteLine("You are not classified on my list");
-}
 
+   
