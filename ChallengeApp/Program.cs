7eebe02715a998a6e8205abd804277f53b048542
Ebby2023 <zibello2010@gmail.com>
2023-05-13 22:21:30 +0100

    Dzien 4 Zadanie

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,13 +1,25 @@
 ﻿
-Console.WriteLine("Hello, World!");
 
+var name1 = "Monika";
+var age = 25;
+var plec = "kobieta";
 
+if (name1 == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (plec != "kobieta" && age < 18)
+{
+    Console.WriteLine("Nepelnoletni mezczyzna");
+}
+else if (plec == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
 
-
-
-
-
-
-
+}
+else
+{
+    Console.WriteLine("Nie wiem kim jestes");
+}
 
 
