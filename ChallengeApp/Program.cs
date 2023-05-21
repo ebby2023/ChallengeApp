f6adaaa31480aac8f6662516635eb6688012c176
Ebby2023 <zibello2010@gmail.com>
2023-05-21 20:45:25 +0100

    Dzien piaty zadanie

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,25 +1,77 @@
 ﻿
 
-var name1 = "Monika";
-var age = 25;
-var plec = "kobieta";
+int number = 5663;
+string numberInString = number.ToString();
+char[] letter = numberInString.ToCharArray();
 
-if (name1 == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (plec != "kobieta" && age < 18)
-{
-    Console.WriteLine("Nepelnoletni mezczyzna");
-}
-else if (plec == "kobieta" && age < 30)
-{
-    Console.WriteLine("Kobieta ponizej 30 lat");
 
-}
-else
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+foreach (char amountNumber in letter)
 {
-    Console.WriteLine("Nie wiem kim jestes");
+    if (amountNumber == '0')
+    {
+        counter0++;
+
+    }
+    else if (amountNumber == '1')
+    {
+        counter1++;
+    }
+    else if (amountNumber == '2')
+    {
+        counter2++;
+    }
+    else if (amountNumber == '3')
+    {
+        counter3++;
+    }
+    else if (amountNumber == '4')
+    {
+        counter4++;
+    }
+
+    else if (amountNumber == '5')
+    {
+        counter5++;
+    }
+    else if (amountNumber == '6')
+    {
+        counter6++;
+    }
+    else if (amountNumber == '7')
+    {
+        counter7++;
+    }
+    else if (amountNumber == '8')
+    {
+        counter8++;
+    }
+    else if (amountNumber == '9')
+    {
+        counter9++;
+    }
 }
+Console.WriteLine($"Your number is {number}");
+Console.WriteLine("0 ==> " + counter0);
+Console.WriteLine("1 ==> " + counter1);
+Console.WriteLine("2 ==> " + counter2);
+Console.WriteLine("3 ==> " + counter3);
+Console.WriteLine("4 ==> " + counter4);
+Console.WriteLine("5 ==> " + counter5);
+Console.WriteLine("6 ==> " + counter6);
+Console.WriteLine("7 ==> " + counter7);
+Console.WriteLine("8 ==> " + counter8);
+Console.WriteLine("9 ==> " + counter9);
 
 
