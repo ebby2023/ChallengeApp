3a1171468b8f689a180d2a3633e3fb137acf4162
Ebby2023 <zibello2010@gmail.com>
2023-06-06 09:55:39 +0100

    Dzien 6 zadanie z klasa Employee - podliczanie punktow pracownikow i znalezienie pracownika z najwieksza liczba punktow

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,38 @@
+﻿
+namespace ChallengeApp;
+
+public class Employee
+{
+    // create list with score number
+    private List<int> score = new List<int>();
+    public Employee(string name, string surname, int age)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Age = age;
+    }
+
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    public int Age { get; set; }
+
+    public int Result
+    {
+        get
+        {
+            return this.score.Sum();
+        }
+
+    }
+
+
+    // method
+
+    public void AddScore(int addscore)
+    {
+        this.score.Add(addscore);
+    }
+}
---
+++ ChallengeApp/User.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ChallengeApp
+{
+    internal class User
+    {
+
+        private List<int> score = new List<int>();
+        // konstruktory
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+
+        }
+        // pola w klasach 
+        public string Login { get; private set; }
+
+        public string Password { get; private set; }
+
+        public int Result
+        {
+            get { return this.score.Sum(); }
+        }
+
+        // Metody 
+        public void AddScore(int addscore)
+        {
+            this.score.Add(addscore);
+        }
+    }
+}
