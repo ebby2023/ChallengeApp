4d030c7c091f3e568d1d4a0d97ccd98930d333a0
Ebby2023 <zibello2010@gmail.com>
2023-06-27 17:08:29 +0100

    Dzien 15. Poprawa klasy Supervisor

--- ChallengeApp/Supervisor.cs
+++ ChallengeApp/Supervisor.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
     internal class Supervisor : IEmployee
 
@@ -21,178 +19,107 @@ namespace ChallengeApp
         public int Age { get; private set; }
 
         public void AddGrade(float grade)
-
         {
             if (grade >= 0 && grade <= 100)
             {
                 this.grades.Add(grade);
             }
             else
-
             {
-                throw new Exception("Invalid great value");
+                throw new Exception("invalid grade value");
             }
-        }
-        public void AddGrade(string grade)
-        {
-            if (float.TryParse(grade, out float newgrades))
-                this.AddGrade(newgrades);
-            switch (grade)
-            {
-                case "6":
-                    this.grades.Add(100);
-                    break;
-                case "-6":
-                case "6-":
-                    this.grades.Add(95);
-                    break;
-                case "+5":
-                case "5+":
-                    this.grades.Add(85);
-                    break;
-                case "5":
-                    this.grades.Add(80);
-                    break;
-                case "-5":
-                case "5-":
-                    this.grades.Add(75);
-                    break;
-                case "4+":
-                case "+4":
-                    this.grades.Add(70);
-                    break;
-                case "4":
-                    this.grades.Add(60);
-                    break;
-                case "-4":
-                case "4-":
-                    this.grades.Add(55);
-                    break;
-                case "+3":
-                case "3+":
-                    this.grades.Add(45);
-                    break;
-                case "3":
-                    this.grades.Add(40);
-                    break;
-                case "-3":
-                case "3-":
-                    this.grades.Add(35);
-                    break;
-                case "2+":
-                case "+2":
-                    this.grades.Add(25);
-                    break;
-                case "2":
-                    this.grades.Add(20);
-                    break;
-                case "-2":
-                case "2-":
-                    this.grades.Add(15);
-                    break;
-                case "1+":
-                case "+1":
-                    this.grades.Add(5);
-                    break;
-                case "1":
-                    this.grades.Add(0);
-                    break;
-                default:
-                    throw new Exception("""Please enter correct value from "1" to "6" thank you.""");
-            }
-
-
-        }
-        public void AddGrade(int grade)
-        {
-            float intGrade = grade;
-            this.grades.Add(intGrade);
         }
         public void AddGrade(double grade)
         {
-            float doubleGrade = (float)grade;
-            this.grades.Add(doubleGrade);
-
+            throw new NotImplementedException();
         }
 
         public void AddGrade(long grade)
         {
-            float longGrade = (float)grade;
-            this.grades.Add(longGrade);
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(int grade)
+        {
+            throw new NotImplementedException();
         }
 
         public void AddGrade(char grade)
         {
-            switch (grade)
-            {
-                case 'A':
-                case 'a':
-                    this.grades.Add(100);
-                    break;
-                case 'B':
-                case 'b':
-                    this.grades.Add(80);
-                    break;
-                case 'C':
-                case 'c':
-                    this.grades.Add(60);
-                    break;
-                case 'D':
-                case 'd':
-                    this.grades.Add(40);
-                    break;
-                case 'E':
-                case 'e':
-                    this.grades.Add(20);
-                    break;
-                default:
-                    throw new Exception("Wrong letter");
-            }
+            throw new NotImplementedException();
         }
 
         public Statistics GetStatistics()
         {
-
-            var statistics = new Statistics();
-            statistics.Max = float.MinValue;
-            statistics.Min = float.MaxValue;
-            statistics.Average = 0;
-
-            foreach (var grade in this.grades)
+            throw new NotImplementedException();
+        }
+        public void AddGrade(string grade)
+        {
+            switch (grade)
             {
-                statistics.Max = Math.Max(statistics.Max, grade);
-                statistics.Min = Math.Min(statistics.Min, grade);
-                statistics.Average += grade;
-
-            }
-            statistics.Average /= this.grades.Count;
-
-            switch (statistics.Average)
-            {
-                case var average when average >= 80:
-                    statistics.AverageLetter = 'A';
+                case "6":
+                    AddGrade(100);
                     break;
-                case var average when average >= 60:
-                    statistics.AverageLetter = 'B';
+                case "-6":
+                case "6-":
+                    AddGrade(95);
                     break;
-                case var average when average >= 40:
-                    statistics.AverageLetter = 'C';
+                case "+5":
+                case "5+":
+                    AddGrade(85);
                     break;
-                case var average when average >= 20:
-                    statistics.AverageLetter = 'D';
+                case "5":
+                    AddGrade(80);
+                    break;
+                case "-5":
+                case "5-":
+                    AddGrade(75);
+                    break;
+                case "+4":
+                case "4+":
+                    AddGrade(65);
+                    break;
+                case "4":
+                    AddGrade(60);
+                    break;
+                case "-4":
+                case "4-":
+                    AddGrade(55);
+                    break;
+                case "+3":
+                case "3+":
+                    AddGrade(45);
+                    break;
+                case "3":
+                    AddGrade(40);
+                    break;
+                case "-3":
+                case "3-":
+                    AddGrade(35);
+                    break;
+                case "+2":
+                case "2+":
+                    AddGrade(25);
+                    break;
+                case "2":
+                    AddGrade(20);
+                    break;
+                case "-2":
+                case "2-":
+                    AddGrade(15);
+                    break;
+                case "+1":
+                case "1+":
+                    AddGrade(5);
+                    break;
+                case "1":
+                    AddGrade(0);
                     break;
                 default:
-                    statistics.Average = 'E';
-                    break;
+                    throw new Exception("invalid grade value");
             }
-
-            return statistics;
-
         }
-
-
     }
 }
-
 
 
